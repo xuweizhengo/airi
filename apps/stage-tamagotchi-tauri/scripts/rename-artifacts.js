92d2f21d7b737c131202bdf066716e3d963fa5df
LemonNeko <chheese048@gmail.com>
2025-04-03 17:56:08 +0800

    chore(tamagotchi,ci): still rename artifacts

---
+++ apps/stage-tamagotchi-tauri/scripts/rename-artifacts.js
@@ -0,0 +1,54 @@
+import fs from 'node:fs'
+import { createRequire } from 'node:module'
+import path from 'node:path'
+import process from 'node:process'
+
+const require = createRequire(import.meta.url)
+const packageJson = require('../package.json')
+
+const version = packageJson.version
+const target = process.argv[2]
+
+console.log('version: ', version)
+console.log('target: ', target)
+
+if (!target) {
+  console.error('Target is required')
+  process.exit(1)
+}
+
+switch (target) {
+  case 'x86_64-pc-windows-msvc':
+    fs.renameSync(
+      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'nsis', `airi-windows-amd64-${version}-setup.exe`),
+      path.join(__dirname, 'bundle', `airi-windows-amd64-${version}-setup.exe`),
+    )
+    break
+  case 'x86_64-unknown-linux-gnu':
+    fs.renameSync(
+      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-x64-${version}.AppImage`),
+      path.join(__dirname, 'bundle', `airi-linux-amd64-${version}.AppImage`),
+    )
+    break
+  case 'aarch64-unknown-linux-gnu':
+    fs.renameSync(
+      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-aarch64-${version}.AppImage`),
+      path.join(__dirname, 'bundle', `airi-linux-arm64-${version}.AppImage`),
+    )
+    break
+  case 'aarch64-apple-darwin':
+    fs.renameSync(
+      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-aarch64-${version}.dmg`),
+      path.join(__dirname, 'bundle', `airi-macos-arm64-${version}.dmg`),
+    )
+    break
+  case 'x86_64-apple-darwin':
+    fs.renameSync(
+      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-x64-${version}.dmg`),
+      path.join(__dirname, 'bundle', `airi-macos-amd64-${version}.dmg`),
+    )
+    break
+  default:
+    console.error('Target is not supported')
+    process.exit(1)
+}
