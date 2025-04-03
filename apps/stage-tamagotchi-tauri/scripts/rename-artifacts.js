d10335376c4d715616a0f23b73622dd19337abc3
LemonNeko <chheese048@gmail.com>
2025-04-03 18:13:22 +0800

    chore(tamagotchi,ci): fix missing dirname

--- apps/stage-tamagotchi-tauri/scripts/rename-artifacts.js
+++ apps/stage-tamagotchi-tauri/scripts/rename-artifacts.js
@@ -8,6 +8,7 @@ const packageJson = require('../package.json')
 
 const version = packageJson.version
 const target = process.argv[2]
+const dirname = import.meta.dirname
 
 console.log('version: ', version)
 console.log('target: ', target)
@@ -20,32 +21,32 @@ if (!target) {
 switch (target) {
   case 'x86_64-pc-windows-msvc':
     fs.renameSync(
-      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'nsis', `airi-windows-amd64-${version}-setup.exe`),
-      path.join(__dirname, 'bundle', `airi-windows-amd64-${version}-setup.exe`),
+      path.join(dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'nsis', `airi-windows-amd64-${version}-setup.exe`),
+      path.join(dirname, 'bundle', `airi-windows-amd64-${version}-setup.exe`),
     )
     break
   case 'x86_64-unknown-linux-gnu':
     fs.renameSync(
-      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-x64-${version}.AppImage`),
-      path.join(__dirname, 'bundle', `airi-linux-amd64-${version}.AppImage`),
+      path.join(dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-x64-${version}.AppImage`),
+      path.join(dirname, 'bundle', `airi-linux-amd64-${version}.AppImage`),
     )
     break
   case 'aarch64-unknown-linux-gnu':
     fs.renameSync(
-      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-aarch64-${version}.AppImage`),
-      path.join(__dirname, 'bundle', `airi-linux-arm64-${version}.AppImage`),
+      path.join(dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'appimage', `airi-linux-aarch64-${version}.AppImage`),
+      path.join(dirname, 'bundle', `airi-linux-arm64-${version}.AppImage`),
     )
     break
   case 'aarch64-apple-darwin':
     fs.renameSync(
-      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-aarch64-${version}.dmg`),
-      path.join(__dirname, 'bundle', `airi-macos-arm64-${version}.dmg`),
+      path.join(dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-aarch64-${version}.dmg`),
+      path.join(dirname, 'bundle', `airi-macos-arm64-${version}.dmg`),
     )
     break
   case 'x86_64-apple-darwin':
     fs.renameSync(
-      path.join(__dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-x64-${version}.dmg`),
-      path.join(__dirname, 'bundle', `airi-macos-amd64-${version}.dmg`),
+      path.join(dirname, 'src-tauri', 'target', target, 'release', 'bundle', 'dmg', `airi-macos-x64-${version}.dmg`),
+      path.join(dirname, 'bundle', `airi-macos-amd64-${version}.dmg`),
     )
     break
   default:
